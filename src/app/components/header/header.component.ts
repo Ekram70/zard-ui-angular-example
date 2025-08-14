f705bb0ee7fc8ac6dd85d0c11781c56854ff1b59
ekram70 <ekramullah70@gmail.com>
2025-08-15 02:45:23 +0600

    feat: updating header component for contact option

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {ThemeToggleBtnComponent} from '../theme-toggle-btn/theme-toggle-btn.component';
+import {ContactComponent} from '../contact/contact.component';
 
 @Component({
   selector: 'app-header',
   imports: [
-    ThemeToggleBtnComponent
+    ThemeToggleBtnComponent,
+    ContactComponent
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
