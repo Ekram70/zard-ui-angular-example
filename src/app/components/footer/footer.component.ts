a92bdcfdd18d609e515c0b473754c7104270446f
ekram70 <ekramullah70@gmail.com>
2025-08-15 13:45:09 +0600

    chore: removed unnecessary imports

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import {ZardButtonComponent} from '@shared/components/button/button.component';
 
 @Component({
   selector: 'app-footer',
   imports: [
-    ZardButtonComponent
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
